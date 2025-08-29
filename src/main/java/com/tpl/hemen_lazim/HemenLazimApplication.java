72e2a3d665685fcd19a5ccdd7399c5ee277fc808
EnesTopal <enes.topal1@gazi.edu.tr>
2025-08-29 12:08:51 +0300

    creation and dependencies

---
+++ src/main/java/com/tpl/hemen_lazim/HemenLazimApplication.java
@@ -0,0 +1,14 @@
+package com.tpl.hemen_lazim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HemenLazimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HemenLazimApplication.class, args);
+        System.out.println("Runs \uD83D\uDC4D");
+    }
+
+}
---
+++ src/test/java/com/tpl/hemen_lazim/HemenLazimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpl.hemen_lazim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HemenLazimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
